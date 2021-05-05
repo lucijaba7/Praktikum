961eb93162b5ae017cf9c4671c3478cfdf845ddd
lucijaba7 <babic.lucija1204@gmail.com>
2021-05-05 11:34:09 +0200

    funciton + testing update

--- Zadace/DZ2_Hotel/index.js
+++ Zadace/DZ2_Hotel/index.js
@@ -1,32 +1,38 @@
-const data = require('./hotel_prices.json')
-
 function priceListFormatter(priceList) {
-  if (!arguments.length) throw Error();
+  if (!arguments.length || Array.isArray(arguments)) throw Error();
 
   // Group by prices
   let groupedList = priceList.reduce((acc, value) => {
     // Group initialization
-    if(!acc[value.price]){
+    if (!acc[value.price]) {
       acc[value.price] = [];
     }
-    
+
     // Grouping
-    acc[value.price].push(value);
+    acc[value.price].push({
+      from: value.from,
+      to: value.to,
+    });
 
     return acc;
   }, {});
-  
+
   // formattedList initialization
   let formattedList = [];
 
   // for every key in groupedList, sorted
-  for (let price of Object.keys(groupedList).sort()){
-    // format sentance for each price
-    formattedList.push(price + ' : ' + groupedList[price].map(x => x.from + ' to ' + x.to).join(', '))
-  }
+  Object.keys(groupedList)
+    .sort()
+    .map((price) => {
+      // format sentance for each price
+      formattedList.push(
+        Number(price).toFixed(1) +
+          " : " +
+          groupedList[price].map((x) => x.from + " to " + x.to).join(", ")
+      );
+    });
 
-  return formattedList
+  console.log(formattedList);
 }
-
 
 module.exports = priceListFormatter;
--- Zadace/DZ2_Hotel/test/priceListFormatter.spec.js
+++ Zadace/DZ2_Hotel/test/priceListFormatter.spec.js
@@ -1,27 +1,51 @@
 const { assert, expect } = require("chai");
 const priceListFormatter = require("../index.js");
-const data = require('../hotel_prices.json')
+const sinon = require("sinon");
+const data = require("../hotel_prices.json");
 
-describe('PriceListFormatter problem', function () {
-    it('priceListFormatter should be a function', function () { 
-        expect(priceListFormatter).to.be.a('function'); 
+describe("PriceListFormatter problem", function () {
+  it("priceListFormatter should be a function", function () {
+    expect(priceListFormatter).to.be.a("function");
+  });
+
+  context("Input", function () {
+    it("Should throw an error if no arguments.", function () {
+      expect(() => priceListFormatter()).to.throw();
     });
-    it('Should throw an error if no arguments.', function(){
-        expect(()=> priceListFormatter()).to.throw();
+    it("Should throw error ig argument is not an array", function () {
+      expect(() => priceListFormatter(1).to.throw());
     });
-    it('priceListFormatter should return an array', function () { 
-        expect(priceListFormatter(data)).to.be.an('array');
+  });
+
+  context("Output", function () {
+    beforeEach(() => {
+      sinon.restore();
     });
-    it('priceListFormatter should return an array with a string "34.5 : 2020-01-01 to 2020-02-01"', function () {
-        expect(priceListFormatter(data)).to.include("34.5 : 2020-01-01 to 2020-02-01");
+
+    const log = sinon.spy(console, "log");
+    priceListFormatter(data);
+    const result = log.args[0][0];
+
+    it("priceListFormatter should log an array", function () {
+      expect(result).to.be.an("array");
     });
-    it('priceListFormatter should return an array with a string "37 : 2020-02-02 to 2020-03-01, 2020-05-15 to 2020-06-15"', function () {
-        expect(priceListFormatter(data)).to.include("37 : 2020-02-02 to 2020-03-01, 2020-05-15 to 2020-06-15");
+
+    it("priceListFormatter should log an array with a lenght of 3", function () {
+      assert.equal(result.length, 3);
     });
-    it('priceListFormatter should return an array with a string "39 : 2020-03-02 to 2020-05-15"', function () {
-        expect(priceListFormatter(data)).to.include("39 : 2020-03-02 to 2020-05-15");
+
+    context("Result", function () {
+      it('priceListFormatter should log an array with a string "34.5 : 2020-01-01 to 2020-02-01"', function () {
+        expect(result).to.include("34.5 : 2020-01-01 to 2020-02-01");
+      });
+      it('priceListFormatter should log an array with a string "37.0 : 2020-02-02 to 2020-03-01, 2020-05-15 to 2020-06-15"', function () {
+        expect(result).to.include(
+          "37.0 : 2020-02-02 to 2020-03-01, 2020-05-15 to 2020-06-15"
+        );
+      });
+      it('priceListFormatter should log an array with a string "39.0 : 2020-03-02 to 2020-05-15"', function () {
+        expect(result).to.include("39.0 : 2020-03-02 to 2020-05-15");
+      });
     });
-    it('priceListFormatter should return an array with a lenght: 3', function () {
-        assert.equal(priceListFormatter(data).length, 3);
-    });
-})
+  });
+});
